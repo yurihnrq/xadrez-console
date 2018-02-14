a850c03add20780319d6fe7c452d19b6d6fc7df1
Yuri Henrique <yuribernardes2010@gmail.com>
2018-02-13 23:41:56 -0200

    Joga especial: roque pequeno e roque grande

--- xadrezConsole/xadrez/Rei.cs
+++ xadrezConsole/xadrez/Rei.cs
@@ -4,8 +4,10 @@ using tabuleiro;
 namespace xadrez {
     class Rei : Peca{
 
-        public Rei(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor) {
+        private PartidaDeXadrez partida;
 
+        public Rei(Tabuleiro tabuleiro, Cor cor, PartidaDeXadrez partida) : base(tabuleiro, cor) {
+            this.partida = partida;
         }
 
         private bool podeMover(Posicao pos) {
@@ -13,50 +15,81 @@ namespace xadrez {
             return p == null || p.cor != cor;
         }
 
+        private bool testeTorreParaRoque(Posicao pos) {
+            Peca p = tabuleiro.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.quantidadeMovimentos == 0;
+        }
+
         public override bool[,] movimentosPossiveis() {
             bool[,] mat = new bool[tabuleiro.linha, tabuleiro.coluna];
             Posicao pos = new Posicao(0, 0);
 
-            //acima
+            // acima
             pos.definirValores(posicao.linha - 1, posicao.coluna);
             if(tabuleiro.posicaoValida(pos) && podeMover(pos)){
                 mat[pos.linha, pos.coluna] = true;
             }
-            //nordeste
+            // NE
             pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
             if (tabuleiro.posicaoValida(pos) && podeMover(pos)) {
                 mat[pos.linha, pos.coluna] = true;
             }
-            //direita
+            // direita
             pos.definirValores(posicao.linha, posicao.coluna + 1);
             if (tabuleiro.posicaoValida(pos) && podeMover(pos)) {
                 mat[pos.linha, pos.coluna] = true;
             }
-            //sudeste
+            // SE
             pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
             if (tabuleiro.posicaoValida(pos) && podeMover(pos)) {
                 mat[pos.linha, pos.coluna] = true;
             }
-            //abaixo
+            // abaixo
             pos.definirValores(posicao.linha + 1, posicao.coluna);
             if (tabuleiro.posicaoValida(pos) && podeMover(pos)) {
                 mat[pos.linha, pos.coluna] = true;
             }
-            // sudoeste
+            // SO
             pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
             if (tabuleiro.posicaoValida(pos) && podeMover(pos)) {
                 mat[pos.linha, pos.coluna] = true;
             }
-            //esquerda
+            // esquerda
             pos.definirValores(posicao.linha, posicao.coluna - 1);
             if (tabuleiro.posicaoValida(pos) && podeMover(pos)) {
                 mat[pos.linha, pos.coluna] = true;
             }
-            //noroeste
+            // NO
             pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
             if (tabuleiro.posicaoValida(pos) && podeMover(pos)) {
                 mat[pos.linha, pos.coluna] = true;
             }
+
+            // #jogadaespecial roque pequeno
+            if(quantidadeMovimentos == 0 && !partida.xeque) {
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                if (testeTorreParaRoque(posT1)) {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if(tabuleiro.peca(p1) == null && tabuleiro.peca(p2) == null) {
+                        mat[posicao.linha, posicao.coluna + 2] = true; 
+                    }
+                }
+            }
+
+            // #jogadaespecial roque grande
+            if (quantidadeMovimentos == 0 && !partida.xeque) {
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                if (testeTorreParaRoque(posT2)) {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tabuleiro.peca(p1) == null && tabuleiro.peca(p2) == null && tabuleiro.peca(p3) == null) {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
+            }
+
             return mat;
         }
 

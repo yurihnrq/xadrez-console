72c8da28f08452ab958f5ec1a09a2b1b9f093739
Yuri Henrique <yuribernardes2010@gmail.com>
2018-02-15 21:21:20 -0200

    Implementado exceções adicionais + ícone do aplicativo(Versão final 1.0)

--- xadrezConsole/Tela.cs
+++ xadrezConsole/Tela.cs
@@ -83,6 +83,13 @@ namespace xadrezConsole {
 
         public static PosicaoXadrez lerPosicaoXadrez() {
             string s = Console.ReadLine();
+            if(s.Length >= 3 || s.Length == 0) {
+                throw new TabuleiroException("Por favor, insira uma posição válida.");
+            } else if (s[0] != 'a' && s[0] != 'b' && s[0] != 'c' && s[0] != 'd' && s[0] != 'e' && s[0] != 'f' && s[0] != 'g' && s[0] != 'h') {
+                throw new TabuleiroException("Por favor, insira uma posição válida.");
+            } else if (s[1] != '1' && s[1] != '2' && s[1] != '3' && s[1] != '4' && s[1] != '5' && s[1] != '6' && s[1] != '7' && s[1] != '8') {
+                throw new TabuleiroException("Por favor, insira uma posição válida.");
+            }
             char coluna = s[0];
             int linha = int.Parse(s[1] + "");
             return new PosicaoXadrez(coluna, linha);
